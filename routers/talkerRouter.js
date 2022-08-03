921e7455586145cc9740ab2896d7d8f3742ffe68
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-08-03 16:47:46 -0300

    feat: Criado arquivo com função que gera token aleatório

--- routers/talkerRouter.js
+++ routers/talkerRouter.js
@@ -1,14 +1,14 @@
 const express = require('express');
-const rescue = require('express-rescue');
+// const rescue = require('express-rescue');
 const { getTalkersList } = require('../services/handleFs');
 
 // const app = express();
 const router = express.Router();
 
-router.get('/', rescue(async (_req, res) => {
+router.get('/', async (_req, res) => {
   const talkers = await getTalkersList();
   return res.status(200).json(talkers);
-}));
+});
 
 // app.use(readTalkersListError);
 
@@ -18,7 +18,7 @@ router.get('/:id', async (req, res) => {
   const talkerId = talkers.find((tlk) => tlk.id === Number(id));
   
   if (!talkerId) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
-  
+
   return res.status(200).json(talkerId);
 });
 

287decfda03397c0bd348846fdb4868320286212
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-08-03 16:42:30 -0300

    feat: Acrescentei rota /login no arquivo index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const talkerRouter = require('./routers/talkerRouter');
+const loginRouter = require('./routers/loginRouter');
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 app.use('/talker', talkerRouter);
+app.use('/login', loginRouter);
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';

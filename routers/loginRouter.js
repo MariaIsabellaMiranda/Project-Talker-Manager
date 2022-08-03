41b1559e3d0140a136ea1efe2b7d2797ef98a9c0
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-08-03 17:29:49 -0300

    feat: Foram importados e colocados em uso os middlewares de valição para email e password

--- routers/loginRouter.js
+++ routers/loginRouter.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const generateToken = require('../services/generateToken');
-// const { checkExistEmailPassword } = require('../middlewares/validateLogin');
+const { checkEmail, checkPassword } = require('../middlewares/validateLogin');
 
 const router = express.Router();
 
-router.post('/', (_req, res) => {
+router.post('/', checkEmail, checkPassword, (req, res) => {
   const token = generateToken();
   res.status(200).json({ token });
 });

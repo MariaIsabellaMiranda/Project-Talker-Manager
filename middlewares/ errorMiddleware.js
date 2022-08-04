1dbd54b388cbc614dbe576e1c0d86f92ab747045
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-08-04 13:28:41 -0300

    Criei middlewares de validação para elementos vindo do body da requisição e o token qu vem do header

--- middlewares/ errorMiddleware.js
+++
@@ -1,6 +0,0 @@
-// const readTalkersListError = (err, _req, res, _next) => (
-//   res.status(500)
-//   .json({ error: `Erro: ${err.message}` })
-// );
-
-// module.exports = { readTalkersListError };
---
+++ middlewares/validateTalker.js
@@ -0,0 +1,76 @@
+const checkToken = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) return res.status(401).json({ message: 'Token não encontrado' });
+
+  if (authorization.length !== 16 || typeof authorization !== 'string') {
+    return res.status(401).json({ message: 'Token inválido' });
+  }
+
+  next();
+};
+
+const checkName = (req, res, next) => {
+  const { name } = req.body;
+
+  if (!name) return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+
+  if (name.length < 3) {
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+
+  next();
+};
+
+const checkAge = (req, res, next) => {
+  const { age } = req.body;
+
+  if (!age || !Number.isInteger(age)) {
+    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  }
+
+  if (age < 18) {
+    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+
+  next();
+};
+
+const checkTalkWatchedAt = (req, res, next) => {
+  const { talk } = req.body;
+  const regexValidate = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
+
+  if (!talk) return res.status(400).json({ message: 'O campo "talk" é obrigatório' });
+
+  if (!talk.watchedAt) {
+    return res.status(400).json({ message: 'O campo "watchedAt" é obrigatório' });
+  }
+
+  if (!regexValidate.test(talk.watchedAt)) {
+    return res.status(400).json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+
+  next();
+};
+
+const checkTalkRate = (req, res, next) => {
+  const { talk } = req.body;
+
+  if (!talk.rate) {
+    return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
+    }
+  
+    if (!Number.isInteger(talk.rate) || talk.rate > 5 || talk.rate < 0) {
+      return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+    }
+
+    next();
+};
+
+module.exports = {
+  checkToken,
+  checkName,
+  checkAge,
+  checkTalkWatchedAt,
+  checkTalkRate,
+};
